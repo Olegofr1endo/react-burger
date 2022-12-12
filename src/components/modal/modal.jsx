0c0a317934e8755a58edcf9022f14f8fbb222e68
Konstantin Solovyev <k1kollov@gmail.com>
2022-12-12 12:16:20 +0300

    Readme

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,7 +3,6 @@ import styles from "./modal.module.css";
 import ModalOverlay from "../modal-overlay/modal-overlay";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
-import { createPortal } from "react-dom";
 
 function Modal({ children, closeModal, isModalOpened }) {
   function escCloseHandler(e) {
